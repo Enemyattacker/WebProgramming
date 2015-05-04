4b9de2ffe6255351f278e25c27f96094354bf376
Enemyattacker <tidehunter.yf@gmail.com>
2015-05-04 13:29:34 +0300

    Изменены имена переменных

--- Timer.js
+++ Timer.js
@@ -1,5 +1,4 @@
 ﻿function time() {	
-
 	day=document.getElementById('day').value;
 	month=document.getElementById('month').value;
 	year=document.getElementById('year').value;
@@ -8,51 +7,54 @@
 	monthpattern=/^(0[1-9]|1[0-2])$/;
 	yearpattern=/^\d\d\d\d/;
 
-	dayproof=daypattern.test(day);
-	monthproof=monthpattern.test(month);
-	yearproof=yearpattern.test(year);
+	dayVerify=daypattern.test(day);
+	monthVerify=monthpattern.test(month);
+	yearVerify=yearpattern.test(year);
 
 
-	if(dayproof==false||monthproof==false||yearproof==false)
+	if(dayVerify==false||monthVerify==false||yearVerify==false)
 	{
 		alert("Неверно введена дата");
 		exit(1);
 	}
 	
-	timeend= new Date(year, month-1, day);
+	endDate= new Date(year, month-1, day);
+	startDate = new Date();
+	dateDifference=endDate-startDate;
 	
-    	today = new Date();
-	
-	if((timeend-today)==0){
+	if(dateDifference==0){
 		alert("Введённая дата наступила!!!");
 		exit(1);
 	}
 
-	if((timeend-today)<0){
+	if(dateDifference<0){
 		alert("Дата отсчёта не может быть раньше сегодняшней даты");
 		exit(1);
 	}
 	
-    	today = Math.floor((timeend-today)/1000);
-    	sec=today%60;
-	today=Math.floor(today/60); 
-    	min=today%60; 
-	today=Math.floor(today/60); 
-    	hour=today%24;
-	today=Math.floor(today/24);
+    daysNumber = Math.floor(dateDifference/1000);
+    secNumber=daysNumber%60;
+	daysNumber=Math.floor(daysNumber/60); 
+    minsNumber=daysNumber%60; 
+	daysNumber=Math.floor(daysNumber/60); 
+    hoursNumber=daysNumber%24;
+	daysNumber=Math.floor(daysNumber/24);
 	
-    	document.getElementById('days').innerHTML= "До введённой даты осталось " + today+ " дней \n"; 
-	if(hour<10){
-	hour='0' + hour;
+	if(hoursNumber<10){
+		hoursNumber='0' + hoursNumber;
 	}
-	document.getElementById('hours').innerHTML=hour+ ":";
-	if(min<10){
-	min='0' + min;
+	
+	if(minsNumber<10){
+		min='0' + minsNumber;
 	}
-	if(sec<10){
-	sec='0'+sec;
+	
+	if(secNumber<10){
+		secNumber='0'+secNumber;
 	}
-	document.getElementById('mins').innerHTML=min+ ":";
-	document.getElementById('secs').innerHTML=sec;
+	
+	document.getElementById('days').innerHTML= "До введённой даты осталось " + daysNumber+ " дней \n"; 
+	document.getElementById('hours').innerHTML=hoursNumber+ ":";
+	document.getElementById('mins').innerHTML=minsNumber+ ":";
+	document.getElementById('secs').innerHTML=secNumber;
 	setTimeout("time()",1000);
 }
