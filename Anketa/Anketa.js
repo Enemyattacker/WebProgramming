83623ea404461473b105a8ce9d44a81d9ffa6f35
Enemyattacker <tidehunter.yf@gmail.com>
2015-05-07 22:45:15 +0300

    no message

---
+++ Anketa/Anketa.js
@@ -0,0 +1,67 @@
+﻿function regExp()
+{
+	login=document.getElementById('first').value;
+	email=document.getElementById('second').value;
+	phone1=document.getElementById('third').value;
+	phone2=document.getElementById('fourth').value;
+	seven=document.getElementById('seven').value;
+	bank1=document.getElementById('five').value;
+	bank2=document.getElementById('six').value;
+	bank3=document.getElementById('nine').value;
+	bank4=document.getElementById('eigth').value;
+	day=document.getElementById('day').value;
+	month=document.getElementById('month').value;
+	year=document.getElementById('year').value;
+	
+	loginpattern=/^[a-z0-9_-]{4,16}$/i;
+	emailpattern=/^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/
+	phone1pattern=/^[0-9]{3}$/;
+	phone2pattern=/^[0-9]{5,7}$/;
+	bankpattern=/^[0-9]{4}$/;
+	daypattern=/^(0[1-9]|[12][0-9]|3[01])$/;
+	monthpattern=/^(0[1-9]|1[0-2])$/;
+	yearpattern=/^(19)\d\d|(20)[0][0-9]$/;
+
+	logproof=loginpattern.test(login);
+	emailproof=emailpattern.test(email);
+	phone1proof=phone1pattern.test(phone1);
+	phone2proof=phone2pattern.test(phone2);
+	bank1proof=bankpattern.test(bank1);
+	bank2proof=bankpattern.test(bank2);
+	bank3proof=bankpattern.test(bank3);
+	bank4proof=bankpattern.test(bank4);
+	dayproof=daypattern.test(day);
+	monthproof=monthpattern.test(month);
+	yearproof=yearpattern.test(year);
+
+	if(logproof==false)
+	{
+		alert("Неверно введён логин");
+	}
+	
+	if(emailproof==false)
+	{
+		alert("Неверно введён E-mail");
+	}
+	
+	if(phone1proof==false||phone2proof==false)
+	{
+		alert("Неверно введён номер телефона");
+	}
+
+	if(bank1proof==false||bank2proof==false||bank3proof==false||bank4proof==false)
+	{
+		alert("Неверно введён номер банковской карты");
+	}
+
+	if(dayproof==false||monthproof==false||yearproof==false)
+	{
+		alert("Неверно введена дата");
+	}
+
+	if(logproof==true&&emailproof==true&&phone1proof==true&&phone2proof==true&&bank1proof==true&&bank2proof==true&&bank3proof==true&&bank4proof==true&&dayproof==true&&monthproof==true&&yearproof==true){
+		alert("Логин " + login + "\nE-Mail " + email + "\nТелефон" + seven +"("+phone1+")" + phone2 + "\nКарта " + bank1 + " " + bank2 + " " + bank3 + " " + bank4 + "\nДата рождения " + day + "." + month + "." + year);
+	}
+	
+
+}
---
+++ Perevod/SistemiSchisleniya.js
@@ -0,0 +1,77 @@
+﻿function chck(){
+
+	number=parseInt(document.getElementById('first').value);
+
+	if((document.getElementById('select').options.selectedIndex)==1){
+		var num_pattern=/[^01]/;
+		var proof=num_pattern.test(number);
+	}
+
+	if((document.getElementById('select').options.selectedIndex)==2){
+		var num_pattern=/[^0-7]/;
+		var proof=num_pattern.test(number);
+	}
+
+	if((document.getElementById('select').options.selectedIndex)==3){
+		var num_pattern=/[^0-9]/;
+		var proof=num_pattern.test(number);
+	}
+
+	if((document.getElementById('select').options.selectedIndex)==4){
+		var num_pattern=/[^0-9A-Fa-f]/;
+		var proof=num_pattern.test(number);
+	}
+
+	if(proof==true){
+
+			alert("Число введено неправильно");
+			document.getElementById('second').style.visibility="hidden"; 
+			document.getElementById('third').style.visibility="hidden"; 
+			document.getElementById('fourth').style.visibility="hidden"; 	
+	}
+	
+	else{
+
+		document.getElementById('second').style.visibility="visible"; 
+		document.getElementById('third').style.visibility="visible"; 
+		document.getElementById('fourth').style.visibility="visible"; 
+
+		if((document.getElementById('select').options.selectedIndex)==1){
+
+			number8=parseInt(number, 2).toString(8);
+			number10=parseInt(number, 2).toString(10);
+			number16=parseInt(number, 2).toString(16);
+			document.getElementById('second').value=number8+ " в восьмеричной с/с";
+			document.getElementById('third').value=number10+ " в десятичной с/с";
+			document.getElementById('fourth').value=number16+ " в шестнадцатиричной с/с";
+		}
+
+		if((document.getElementById('select').options.selectedIndex)==2){
+			number2=parseInt(number, 8).toString(2);
+			number10=parseInt(number, 8).toString(10);
+			number16=parseInt(number, 8).toString(16);
+			document.getElementById('second').value=number2+ " в двоичной с/с";
+			document.getElementById('third').value=number10+ " в десятичной с/с";
+			document.getElementById('fourth').value=number16+ " в шестнадцатиричной с/с";
+		}
+
+		if((document.getElementById('select').options.selectedIndex)==3){
+			number2=parseInt(number, 10).toString(2);
+			number8=parseInt(number, 10).toString(8);
+			number16=parseInt(number, 10).toString(16);
+			document.getElementById('second').value=number2+ " в двоичной с/с";
+			document.getElementById('third').value=number8+ " в восьмеричной с/с";
+			document.getElementById('fourth').value=number16+ " в шестнадцатиричной с/с";
+		}
+
+		if((document.getElementById('select').options.selectedIndex)==4){
+			number2=parseInt(number, 16).toString(2);
+			number8=parseInt(number, 16).toString(8);
+			number10=parseInt(number, 16).toString(10);
+			document.getElementById('second').value=number2+ " в двоичной с/с";
+			document.getElementById('third').value=number8+ " в восьмеричной с/с";
+			document.getElementById('fourth').value=number10+ " в десятичной с/с";
+		}
+	}
+
+}
